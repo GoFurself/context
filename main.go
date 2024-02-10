62bfc391353e6189f0b459a13c992fac81a32e17
= <=>
2024-02-10 14:31:10 +0200

    Fixit

--- main.go
+++ main.go
@@ -10,11 +10,8 @@ import (
 
 // This program sends multiple requests to different urls concurrently and cancels the requests after one of them is done.
 // It main purpose is to show how to use context to cancel requests and how to use WaitGroup to wait for all requests to finish.
-// The structure can be used as a load balancer
+// The idea can be used for example as a load balancer
 func main() {
-
-	wg := sync.WaitGroup{}
-	ctx, cancel := context.WithCancel(context.Background())
 
 	urls := []string{
 		"https://www.tumblr.com",
@@ -28,38 +25,37 @@ func main() {
 		"https://www.pinterest.com",
 	}
 
-	for i := 0; i < len(urls); i++ {
+	wg := sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(context.Background())
 
+	for i := 0; i < len(urls); i++ {
 		wg.Add(1)
 		go func(x int) {
-			// If the context is cancelled, the request will be cancelled as well
-			RequestWithContext(ctx, &wg, urls[x], cancel)
+			defer wg.Done()
+			if err := DoGetRequestWithContext(ctx, urls[x]); err == nil {
+				cancel()
+				fmt.Println("Url: " + urls[x] + " got 200 status first. Cancelling other requests.")
+			} else {
+				fmt.Println("Error: ", err)
+			}
 		}(i)
 	}
-
 	wg.Wait()
 }
 
-func RequestWithContext(ctx context.Context, wg *sync.WaitGroup, url string, cancel context.CancelFunc) error {
-	defer wg.Done()
+func DoGetRequestWithContext(ctx context.Context, url string) error {
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		fmt.Println("Error: ", err)
 		return err
 	}
-	client := &http.Client{}
-	start := time.Now()
+	client := &http.Client{
+		Timeout: 2 * time.Second,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error: ", err)
 		return err
 	}
-	duration := time.Since(start)
-
 	defer resp.Body.Close()
-	defer cancel()
-	fmt.Println("Url: "+url+" got response status:", resp.Status, "in", duration)
-
 	return nil
 }
